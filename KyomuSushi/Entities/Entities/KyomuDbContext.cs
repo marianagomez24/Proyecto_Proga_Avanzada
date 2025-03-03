7103cdd564e3e04b7c46962208dd96bded11e063
Fabiana Jara <fabianajara.u@gmail.com>
2025-03-02 18:57:49 -0600

    Actualización de Entities
    Se conecto la nueva instancia de la bdd.

--- KyomuSushi/Entities/Entities/KyomuDbContext.cs
+++ KyomuSushi/Entities/Entities/KyomuDbContext.cs
@@ -4,13 +4,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Entities.Entities;
 
-public partial class KyomuDbContext : DbContext
+public partial class KyomudbContext : DbContext
 {
-    public KyomuDbContext()
+    public KyomudbContext()
     {
     }
 
-    public KyomuDbContext(DbContextOptions<KyomuDbContext> options)
+    public KyomudbContext(DbContextOptions<KyomudbContext> options)
         : base(options)
     {
     }
@@ -35,13 +35,13 @@ public partial class KyomuDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=kyomu-server.database.windows.net;Database=KyomuDB;User Id=kyomuAdmin;Password=kyomu123!;");
+        => optionsBuilder.UseSqlServer("Server=34.31.11.26;Database=kyomudb;User Id=sqlserver;Password=kyomu123!;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Categorium>(entity =>
         {
-            entity.HasKey(e => e.IdCategoria).HasName("PK__Categori__02AA0785A6EE078D");
+            entity.HasKey(e => e.IdCategoria).HasName("PK__Categori__02AA07853A752253");
 
             entity.Property(e => e.IdCategoria).HasColumnName("ID_Categoria");
             entity.Property(e => e.Descripcion).HasMaxLength(255);
@@ -50,7 +50,7 @@ public partial class KyomuDbContext : DbContext
 
         modelBuilder.Entity<DetallePedido>(entity =>
         {
-            entity.HasKey(e => e.IdDetallePedido).HasName("PK__Detalle___5ED5F41E993C2D73");
+            entity.HasKey(e => e.IdDetallePedido).HasName("PK__Detalle___5ED5F41ED9B9D065");
 
             entity.ToTable("Detalle_Pedido");
 
@@ -61,17 +61,17 @@ public partial class KyomuDbContext : DbContext
             entity.HasOne(d => d.IdPedidoNavigation).WithMany(p => p.DetallePedidos)
                 .HasForeignKey(d => d.IdPedido)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Detalle_P__ID_Pe__151B244E");
+                .HasConstraintName("FK__Detalle_P__ID_Pe__4AB81AF0");
 
             entity.HasOne(d => d.IdPlatilloNavigation).WithMany(p => p.DetallePedidos)
                 .HasForeignKey(d => d.IdPlatillo)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Detalle_P__ID_Pl__160F4887");
+                .HasConstraintName("FK__Detalle_P__ID_Pl__4BAC3F29");
         });
 
         modelBuilder.Entity<MetodoPago>(entity =>
         {
-            entity.HasKey(e => e.IdMetodo).HasName("PK__Metodo_P__AE866C410DFE0CD4");
+            entity.HasKey(e => e.IdMetodo).HasName("PK__Metodo_P__AE866C412776D601");
 
             entity.ToTable("Metodo_Pago");
 
@@ -81,7 +81,7 @@ public partial class KyomuDbContext : DbContext
 
         modelBuilder.Entity<Pago>(entity =>
         {
-            entity.HasKey(e => e.IdPago).HasName("PK__Pago__AE88B429E51CD776");
+            entity.HasKey(e => e.IdPago).HasName("PK__Pago__AE88B4298B35B263");
 
             entity.ToTable("Pago");
 
@@ -96,17 +96,17 @@ public partial class KyomuDbContext : DbContext
             entity.HasOne(d => d.IdMetodoNavigation).WithMany(p => p.Pagos)
                 .HasForeignKey(d => d.IdMetodo)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Pago__ID_Metodo__1BC821DD");
+                .HasConstraintName("FK__Pago__ID_Metodo__5165187F");
 
             entity.HasOne(d => d.IdPedidoNavigation).WithMany(p => p.Pagos)
                 .HasForeignKey(d => d.IdPedido)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Pago__ID_Pedido__1AD3FDA4");
+                .HasConstraintName("FK__Pago__ID_Pedido__5070F446");
         });
 
         modelBuilder.Entity<Pedido>(entity =>
         {
-            entity.HasKey(e => e.IdPedido).HasName("PK__Pedido__FD768070E771745C");
+            entity.HasKey(e => e.IdPedido).HasName("PK__Pedido__FD768070626FD5B1");
 
             entity.ToTable("Pedido");
 
@@ -125,12 +125,12 @@ public partial class KyomuDbContext : DbContext
             entity.HasOne(d => d.IdUsuarioNavigation).WithMany(p => p.Pedidos)
                 .HasForeignKey(d => d.IdUsuario)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Pedido__ID_Usuar__114A936A");
+                .HasConstraintName("FK__Pedido__ID_Usuar__46E78A0C");
         });
 
         modelBuilder.Entity<Platillo>(entity =>
         {
-            entity.HasKey(e => e.IdPlatillo).HasName("PK__Platillo__28C8FA38DB332F1E");
+            entity.HasKey(e => e.IdPlatillo).HasName("PK__Platillo__28C8FA38F514FB5B");
 
             entity.ToTable("Platillo");
 
@@ -143,12 +143,12 @@ public partial class KyomuDbContext : DbContext
 
             entity.HasOne(d => d.IdCategoriaNavigation).WithMany(p => p.Platillos)
                 .HasForeignKey(d => d.IdCategoria)
-                .HasConstraintName("FK__Platillo__ID_Cat__07C12930");
+                .HasConstraintName("FK__Platillo__ID_Cat__3D5E1FD2");
         });
 
         modelBuilder.Entity<Reseña>(entity =>
         {
-            entity.HasKey(e => e.IdReseña).HasName("PK__Reseña__B4FBAB75C4E9A625");
+            entity.HasKey(e => e.IdReseña).HasName("PK__Reseña__B4FBAB75FFBA62EE");
 
             entity.ToTable("Reseña");
 
@@ -160,17 +160,17 @@ public partial class KyomuDbContext : DbContext
             entity.HasOne(d => d.IdPlatilloNavigation).WithMany(p => p.Reseñas)
                 .HasForeignKey(d => d.IdPlatillo)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Reseña__ID_Plati__208CD6FA");
+                .HasConstraintName("FK__Reseña__ID_Plati__5629CD9C");
 
             entity.HasOne(d => d.IdUsuarioNavigation).WithMany(p => p.Reseñas)
                 .HasForeignKey(d => d.IdUsuario)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Reseña__ID_Usuar__1F98B2C1");
+                .HasConstraintName("FK__Reseña__ID_Usuar__5535A963");
         });
 
         modelBuilder.Entity<Rol>(entity =>
         {
-            entity.HasKey(e => e.IdRol).HasName("PK__Rol__202AD2208DDC13D5");
+            entity.HasKey(e => e.IdRol).HasName("PK__Rol__202AD220B6AFC835");
 
             entity.ToTable("Rol");
 
@@ -181,11 +181,11 @@ public partial class KyomuDbContext : DbContext
 
         modelBuilder.Entity<Usuario>(entity =>
         {
-            entity.HasKey(e => e.IdUsuario).HasName("PK__Usuario__DE4431C5C979ED4A");
+            entity.HasKey(e => e.IdUsuario).HasName("PK__Usuario__DE4431C5A7C51440");
 
             entity.ToTable("Usuario");
 
-            entity.HasIndex(e => e.CorreoElectronico, "UQ__Usuario__531402F3E5738357").IsUnique();
+            entity.HasIndex(e => e.CorreoElectronico, "UQ__Usuario__531402F39293E782").IsUnique();
 
             entity.Property(e => e.IdUsuario).HasColumnName("ID_Usuario");
             entity.Property(e => e.Contraseña).HasMaxLength(255);
@@ -199,7 +199,7 @@ public partial class KyomuDbContext : DbContext
             entity.HasOne(d => d.IdRolNavigation).WithMany(p => p.Usuarios)
                 .HasForeignKey(d => d.IdRol)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Usuario__ID_Rol__0B91BA14");
+                .HasConstraintName("FK__Usuario__ID_Rol__412EB0B6");
         });
 
         OnModelCreatingPartial(modelBuilder);
